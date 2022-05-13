3ff961699f0e5d0bae87510cf4500c146811c44e
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-05-12 19:51:45 -0500

    Operador Arigmetico Suma

--- main.js
+++ main.js
@@ -1,11 +1,37 @@
-let nombre = "Miguel Angel";
+let x = 10;
+let y = 3;
+let res = 0;
 
+let sel = document.querySelectorAll("em");
+sel[0].insertAdjacentText("beforeend", x);
+sel[1].insertAdjacentText("beforeend", y);
+sel[2].insertAdjacentText("beforeend", res);
 
 // Significado
-// let nombreCompleto = nombre + " Castro Escamilla";
+res = x + y;
+document.body.insertAdjacentHTML("beforeend", `
+    <div>
+        <b>Significado</b>
+    </div>
+    <hr>
+    <div>
+        <em>res = x + y;</em>
+        <br>
+        <mark>res</mark> : <em>${res}</em>
+    </div>
+`);
 
-//Abreviado
-// nombre += " Castro Escamilla";
-
-
-document.body.insertAdjacentText("beforebegin", nombre);
+// Abreviado
+x += y;
+document.body.insertAdjacentHTML("beforeend", `
+    <br>
+    <div>
+        <b>Abreviado</b>
+    </div>
+    <hr>
+    <div>
+        <em>x += y;</em>
+        <br>
+        <mark>x</mark> : <em>${x}</em>
+    </div>
+`);
