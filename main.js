a457d8323d73552d3f900170fa7a46e282746ce8
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-05-12 19:27:52 -0500

    Operador de Adicion

---
+++ main.js
@@ -0,0 +1,11 @@
+let nombre = "Miguel Angel";
+
+
+// Significado
+// let nombreCompleto = nombre + " Castro Escamilla";
+
+//Abreviado
+// nombre += " Castro Escamilla";
+
+
+document.body.insertAdjacentText("beforebegin", nombre);
